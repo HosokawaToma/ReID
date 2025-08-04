e06071da2585b0ef3acd14e21fc638c070c7a131
HosokawaToma <toma07161@gmail.com>
2025-08-04 21:47:25 +0900

    fix: CLAHEがカラー画像に対応していない問題を修正

--- src/managers/pre_processing_manager.py
+++ src/managers/pre_processing_manager.py
@@ -7,5 +7,8 @@ class PreProcessingManager:
         pass
 
     def clahe(self, image: np.ndarray) -> np.ndarray:
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
-        return clahe.apply(image)
+        image = clahe.apply(image)
+        image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
+        return image
