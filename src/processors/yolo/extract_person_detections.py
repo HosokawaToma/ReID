0928d1cafd9d6ffc601f99f02a82ccafd342e431
HosokawaToma <toma07161@gmail.com>
2025-08-18 16:41:59 +0900

    fix: 人物検出のインデックスを削除

--- src/processors/yolo/extract_person_detections.py
+++ src/processors/yolo/extract_person_detections.py
@@ -34,7 +34,7 @@ class ExtractPersonDetectionsProcessor:
         result = results[0]
 
         if result.boxes is not None:
-            for i, box in enumerate(result.boxes):
+            for box in result.boxes:
                 box_id = int(box.id[0].cpu().numpy())
                 box_xyxy = box.xyxy[0].cpu().numpy()
                 box_xywh = box.xywh[0].cpu().numpy()
