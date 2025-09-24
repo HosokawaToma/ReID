ccddb04323c55cc8ec100a4931acf19eef27cebf
HosokawaToma <toma07161@gmail.com>
2025-09-24 19:14:07 +0900

    feat: add service to encode image

--- camra_app/application/api/__init__.py
+++ camra_app/application/api/__init__.py
@@ -3,6 +3,7 @@ import requests
 
 from entities.api.person_crop_images_metadata import \
     EntityApiPersonCropImagesMetadata
+from service.image import ServiceImage
 
 
 class ApplicationApi:
@@ -11,10 +12,13 @@ class ApplicationApi:
         self.person_cropped_images_url = "http://" + self.server_ip + "/identify_person"
 
     def post_person_cropped_images(self, person_cropped_images: list[np.ndarray], metadata: EntityApiPersonCropImagesMetadata):
-        files = [
-            ("images", (f"person_cropped_image_{i}.jpg", person_cropped_image, "image/jpeg"))
-            for i, person_cropped_image in enumerate(person_cropped_images)
-        ]
+        if len(person_cropped_images) == 0:
+            return
+        files = []
+        for i, person_cropped_image in enumerate(person_cropped_images):
+            image_bytes = ServiceImage.encode_image(person_cropped_image)
+            files.append(
+                ("images", (f"person_cropped_image_{i}.jpg", image_bytes, "image/jpeg")))
         data = {
             "camera_id": metadata.camera_id,
             "view_id": metadata.view_id,
